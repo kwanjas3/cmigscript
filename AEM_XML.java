a5c028721e0988266b54ba7891a91cdad38c54bc
vincentleungdev <63195106+vincentleungdev@users.noreply.github.com>
2020-10-06 14:49:16 -0400

    Update AEM_XML.java

--- AEM_XML.java
+++ AEM_XML.java
@@ -45,18 +45,6 @@ public class AEM_XML {
 
     public static void displayDirectoryFiles(File dir) {
         nodeName.add("jcr:content");
-        /*
-
- 
-
-        nodeName.add("admission-requirements");
-        nodeName.add("courses");
-        nodeName.add("costs");
-        nodeName.add("pathways");
-        nodeName.add("contact-us");
-        nodeName.add("apply-now");
-        nodeName.add("program-learning-outcomes");
-         */
         try {
             File[] files = dir.listFiles();
             for (File file : files) {
@@ -65,19 +53,16 @@ public class AEM_XML {
                     displayDirectoryFiles(file);
                     // displayDirectoryContents(file);
                 } else {
+                 /** TODO rename contenttEST6265 to folder you are trying to iterate through **/             
                     if (file.toString().endsWith(".content.xml") && file.toString().contains("contentTest6265")) {
                         // System.out.println("     file:" + file.getCanonicalPath());
                         DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
                         DocumentBuilder b = f.newDocumentBuilder();
                         org.w3c.dom.Document doc = b.parse(new ByteArrayInputStream(readTextFile(file.toString()).getBytes()));
 
- 
-
                         Element root = doc.createElement("root");
                         root.setAttribute("jcr:primaryType", "nt:unstructured");
                         root.setAttribute("sling:resourceType", "wcm/foundation/components/responsivegrid");
-
- 
 
                         Element responsiveGrid = doc.createElement("responsivegrid");
                         responsiveGrid.setAttribute("jcr:primaryType", "nt:unstructured");
@@ -88,77 +73,29 @@ public class AEM_XML {
                         NodeList nList = doc.getElementsByTagName("jcr:content");
                         for (int temp = 0; temp < nList.getLength(); temp++) {
 
- 
-
                             Node nNode = nList.item(temp);
                             if (nNode.getNodeName().equals("jcr:content")) {
-                                //System.out.println("\nCurrent Element :" + nNode.getNodeName());
-
- 
-
-                                //  responsiveGrid.appendChild(nNode);                           
-                                // nNode.appendChild(root);
+                                //System.out.println("\nCurrent Element :" + nNode.getNodeName())
                                 NodeList subList = nNode.getChildNodes();
-
- 
 
                                 for (int j = 0; j < subList.getLength(); j++) {
                                     if (subList.item(j).getNodeType() == Node.ELEMENT_NODE
                                             && !subList.item(j).getNodeName().equals("root")) {
                                         Node jNode = subList.item(j);
                                         added = true;
-                                        //nNode.appendChild(jNode);
                                         responsiveGrid.appendChild(jNode);
-                                        System.out.println("added parent node: " + jNode.getNodeName());
-
- 
-
+                                        System.out.println("added parent node: " + jNode.getNodeName())
                                     }
-
- 
-
                                 }
                             }
                             if (added) {
                                 System.out.println("file> " + file);
-                                nNode.appendChild(root);
-
- 
-
+                                nNode.appendChild(root); 
                             }
-
- 
-
-                            //nNode.getParentNode().appendChild(root);
-                            //  doc.removeChild(nNode);
-
- 
-
-                            /* NodeList subList = nNode.getChildNodes();
-                            for (int j = 0; j < subList.getLength(); j++) {
-
- 
-
-                                Node jNode = subList.item(j);
-                                System.out.println("");
-                                if (jNode.getNodeType() == Node.ELEMENT_NODE) {
-
- 
-
-                                    //System.out.println("jNode> " + jNode.getNodeName());
-                                    if (nodeName.contains(jNode.getNodeName())) {
-                                        root.appendChild(responsiveGrid);
-                                        responsiveGrid.appendChild(jNode);
-                                        nNode.appendChild(root);
-                                        System.out.println("added parent node: " + jNode.getNodeName());
-                                    }
-                                }
-                            }*/
+                   
                             TransformerFactory transformerFactory = TransformerFactory.newInstance();
                             Transformer transformer = transformerFactory.newTransformer();
                             DOMSource source = new DOMSource(doc);
-
- 
 
                             StreamResult result = new StreamResult(file);
                             transformer.transform(source, result);
@@ -173,14 +110,11 @@ public class AEM_XML {
         }
     }
 
- 
 
     public static String readTextFile(String fileName) throws IOException {
         String content = new String(Files.readAllBytes(Paths.get(fileName)));
         return content;
     }
-
- 
 
     public static List<String> readTextFileByLines(String fileName) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get(fileName));
@@ -188,7 +122,6 @@ public class AEM_XML {
     }
 
  
-
     public static void writeToTextFile(String fileName, String content) throws IOException {
         Files.write(Paths.get(fileName), content.getBytes(), StandardOpenOption.CREATE);
     }
